9963db9e135b6dbadafe99ea09371c95fe6199c1
William Desportes <williamdes@wdes.fr>
2015-06-07 00:00:00 +0200

    Remove some files

--- src/main/java/net/minecraft/bootstrap/FatalBootstrapError.java
+++
@@ -1,7 +0,0 @@
-package net.minecraft.bootstrap;
-
-public class FatalBootstrapError extends RuntimeException {
-    public FatalBootstrapError(final String reason) {
-        super(reason);
-    }
-}
