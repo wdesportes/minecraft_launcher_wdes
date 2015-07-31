88a9595c70c9260fa2d365db5ecf364b4f464ccb
William Desportes <williamdes@wdes.fr>
2015-07-31 22:45:00 +0200

    Batch commit of files modified at 2015-07-31T22:45

--- src/main/java/fr/wdes/process/ProcessMonitorThread.java
+++ src/main/java/fr/wdes/process/ProcessMonitorThread.java
@@ -5,12 +5,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import fr.wdes.Launcher;
 import fr.wdes.logger;
+
 
 
 public class ProcessMonitorThread extends Thread {
     private final JavaProcess process;
-
     public ProcessMonitorThread(final JavaProcess process) {
         this.process = process;
     }
@@ -24,7 +26,32 @@ public class ProcessMonitorThread extends Thread {
         while(process.isRunning() )
             try {
                 while((line = buf.readLine()) != null) {
-                	logger.info("Client> " + line);
+                	logger.info("[Minecraft] " + line);
+                	if(line.contains("Setting user")){
+                		Launcher.getInstance().getLauncherPanel().progressBar.setText("[Joueur] Démarrage en cours ...");
+                	}
+                	else if(line.contains("LWJGL")){
+                		Launcher.getInstance().getLauncherPanel().progressBar.setText("[LWJGL] Démarrage en cours ...");
+                	}
+                	else if(line.contains("Reloading ResourceManager")){
+                		Launcher.getInstance().getLauncherPanel().progressBar.setText("[Ressources] Redémarrage en cours ...");
+                	}
+                	else if(line.contains("Sound engine started")){
+                		Launcher.getInstance().getLauncherPanel().progressBar.setText("[Son] Démarré !!");
+                	}
+                	else if(line.contains("Created") && line.contains("textures")){
+                		Launcher.getInstance().getLauncherPanel().progressBar.setText("[Textures] Création en cours ...");
+                	}
+                	else if(line.contains("MCO Availability Checker")){
+                		Launcher.getInstance().getLauncherPanel().progressBar.setText("[Realms] Vérification ...");
+                	}
+                	else if(line.contains("Starting integrated minecraft server") || line.contains("Generating keypair")){
+                		Launcher.getInstance().getLauncherPanel().progressBar.setText("[Jeu] Bon jeu !!");
+                	}
+                	else if(line.contains("Stopping!")){
+                		Launcher.getInstance().getLauncherPanel().progressBar.setText("[Jeu] Arrêt en cours ...");
+                	}
+                	
                     process.getSysOutLines().add(line);
                 }
             }
